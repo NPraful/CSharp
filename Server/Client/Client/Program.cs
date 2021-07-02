c295acff75e1407e34ac9c03a91a9b5f75cdf0ce
NARKHEDE PRAFUL <PRAFUL.NARKHEDE@kohler.com>
2021-07-02 13:09:05 +0530

    Initial Check-in

---
+++ Server/Client/Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client();
+        }
+
+        static void Client()
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            while (true)
+            {
+                try
+                {
+                    tcpclnt.Connect("192.168.100.7", 8000);
+                    Console.WriteLine("Connected...");
+
+                    Console.Write("\n Enter the string to be sent: ");
+
+                    String str = Console.ReadLine();
+                    Stream stm = tcpclnt.GetStream();
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] ba = asen.GetBytes(str);
+                    Console.WriteLine("Sending...");
+
+                    stm.Write(ba, 0, ba.Length);
+
+                    byte[] bb = new byte[1024];
+                    int k = stm.Read(bb, 0, 1024);
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        Console.Write(Convert.ToChar(bb[i]));
+                    }
+
+                    tcpclnt.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error..... " + e.StackTrace);
+                }
+            }
+        }
+
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serve();
+        }
+
+        static void Serve()
+        {
+            IPAddress ipAdress = IPAddress.Parse("192.168.100.7");
+
+            // Initializes the Listener
+            TcpListener myList = new TcpListener(ipAdress, 8000);
+            Socket s;
+            while (true)
+            {
+                try
+                {
+                    // Start Listeneting at the specified port
+                    myList.Start();
+                    s = myList.AcceptSocket();
+                    Console.WriteLine("Server running... @ Port: 8000");
+                    Console.WriteLine("Local End point: " + myList.LocalEndpoint);
+                    Console.WriteLine("Waiting for connections...");
+                    // When accepted
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                    byte[] b = new byte[1024];
+                    int k = s.Receive(b);
+                    Console.WriteLine("Recieved...");
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        Console.Write(Convert.ToChar(b[i]));
+                    }
+
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("Automatic message: " + "String received by server!"));
+                    Console.WriteLine("\n Automatic message sent!");
+
+                    s.Close();
+                    myList.Stop();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error..... " + e.StackTrace);
+                }
+            }
+        }
+    }
+}
